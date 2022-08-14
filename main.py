b396ba9e2dda80f630649dca32dbc60da314743e
cristobalza <cristobalzamas@gmail.com>
2022-08-14 12:16:44 -0400

    finished with CRUD operations

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from random import randrange
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from post import Post
 
 app  = FastAPI()
 
 my_posts = [{'title': 'title of post 1', 'content': 'content of post 1', 'id': 1},
-            {'title': 'favorite foods', 'content': 'I like pizza', 'id': "2"}]
+            {'title': 'favorite foods', 'content': 'I like pizza', 'id': 2}]
 
 @app.get("/")
 def root(): 
@@ -16,7 +16,7 @@ def root():
 def get_posts():
     return {'data': my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code = status.HTTP_201_CREATED)
 def create_posts(post: Post):
     temp = post.dict()
     temp['id'] = randrange(0, 9999999)
@@ -24,11 +24,45 @@ def create_posts(post: Post):
     return {'data': temp}
 
 @app.get('/posts/{id}')
-def get_post(id:int):
-    p = find_post(int(id))
-    return {'post_detail': f'this is your post {p}'}
+def get_post(id:int, response: Response):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id : {id} was not found.")
+    return {'post_detail': f'this is your post {post}'}
 
-def find_post(id):
+@app.delete("/posts/{id}")
+def delete_post(id:int, status_code=status.HTTP_204_NO_CONTENT):
+    # deleting post
+    # find index in the array that has requried id
+    # my_posts.pop(index)
+    index = find_index(id)
+    if index is not None:
+        my_posts.pop(index)
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"id post : {id} was not found.")
+
+@app.put("/posts/{id}")
+def update_post(id:int, post: Post):
+    index = find_index(id)
+    if index is not None:
+        temp = post.dict()
+        temp['id'] = id
+        my_posts[index] = temp
+        return{'data': temp}
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"id post : {id} was not found.")
+
+
+
+def find_post(id:int):
     for p in my_posts:
-        if int(p['id']) == id:
+        if p['id'] == id:
             return p
+
+def find_index(id:int):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+    return None
