71be219f90aa0eba4bbeaf248606ebe4fba82ea4
cristobalza <cristobalzamas@gmail.com>
2022-08-20 12:44:26 -0400

    added Oath2 from fastapi

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,6 @@
 from os import access
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm 
 from sqlalchemy.orm import Session
 from .. import database, schemas, models, utils, oath2
 
@@ -8,9 +9,10 @@ router = APIRouter(
 )
 
 @router.post('/login')
-def login(user_credentials: schemas.UserLogin, 
+def login(user_credentials: OAuth2PasswordRequestForm  = Depends(), 
           db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+    
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
     
     if user is None:
         message = f"Invalid credentials."
