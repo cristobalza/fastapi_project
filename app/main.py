e96b1ce26e0f561689a2193bca74f7b83515ef35
cristobalza <cristobalzamas@gmail.com>
2022-08-24 20:42:01 -0400

    start testing vote.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .routers import post, user, auth
+from .routers import post, user, auth, vote
 
 # Whenever we start the application, SQLAlchemy will do either one of the folowing:
 # 1. If there is a table with the names in models.py, it will do nothing
@@ -13,6 +13,7 @@ app  = FastAPI()
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 @app.get("/")
 def root(): 
