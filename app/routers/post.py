6212abd449ca1a34646f5b007d1ab644291fbf40
cristobalza <cristobalzamas@gmail.com>
2022-08-28 00:11:39 -0400

    setting up alembic

--- app/routers/post.py
+++ app/routers/post.py
@@ -29,7 +29,8 @@ def get_posts(db: Session = Depends(get_db),
     # return results.all()
     
     posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
-        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(
+            models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostOut)
